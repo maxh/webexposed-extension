b874af4b4a1c344372e5f809e4f186d5539df48c
Max Heinritz <maxheinritz@gmail.com>
2012-10-22 18:33:36 -0700

    Add save button, colors

--- client/webexposed.js
+++ client/webexposed.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Logic for WebExposed Chrome extension. Allows users to 
+ * @fileoverview The WebExposed Chrome extension allows users to 
  * organize WebKit bugs labeled with the "WebExposed" keyword.
  * @author meh@chromium.org (Max Heinritz)
  */
@@ -13,6 +13,9 @@
  */
 
 function addColumn(name,type) {
+
+	// Replace spaces with underscores in the column name
+	name = name.replace(/ /g,"-");
 
 	// Create cols for matching CSS to columns
 	// Note: Bugzilla buglist tables contain at most 100 bugs per table,
@@ -28,12 +31,12 @@ function addColumn(name,type) {
 	var headerLists = document.getElementsByClassName('bz_buglist_header');
 	for (var i = headerLists.length - 1; i >= 0; i--) {
 		var th = document.createElement('th');
-		th.className = 'we_' + name.replace(/ /g,"-") + '_header';
+		th.className = 'we_' + name + '_header';
 		th.innerHTML = name;
 		headerLists[i].appendChild(th);
 	}
 
-	addUIElement(name,type);
+	addUIElementToAllRows(name,type);
 }
 
 /**
@@ -44,12 +47,17 @@ function addColumn(name,type) {
  * @param {string} type This is the {@code HTML} {@code input} type for the
  *     attribute. (eg {@code checkbox} for 'hidden')
  */
-function addUIElement(name,type) {
+function addUIElementToAllRows(name,type) {
 	// Add UI elements to modify the attribute
 	var rows = document.getElementsByClassName('bz_bugitem');
 
 	for (var i = rows.length - 1; i >= 0; i--) {
+		// Extract the bug id from the link in the first cell in this row
+		var bugId = rows[i].cells[0].getElementsByTagName('a')[0].innerHTML;
+
 		var td = document.createElement('td');
+		td.className = 'we_' + name.replace(/ /g,"-") + '_column';
+		
 		var input;
 
 		// Most inputs will be input elements, except for textareas.
@@ -65,12 +73,26 @@ function addUIElement(name,type) {
 			}
 		}
 
+		input.name = name + "^"+ bugId;
+
 		td.appendChild(input);
 		rows[i].appendChild(td);
 	}
 }
 
+function addSaveButton() {
+	var header = document.getElementById('header');
+	var button = document.createElement('input');
+	button.type = 'submit';
+	button.value = 'Save WebExposed Attributes';
+	button.id = 'we_button';
+
+	header.appendChild(button);
+}
+
+// only run the extension on the WebExposed bug list
 if (document.URL.indexOf('keywords=WebExposed') != -1) {
+	addSaveButton();
 	addColumn('hidden','checkbox');
 	addColumn('important','checkbox');
 	addColumn('crbug id','text');
